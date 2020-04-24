e0a37e987052b5cc870b5958fce466bc715ffc53
Dominick C. Pastore <dominickpastore@dcpx.org>
2020-04-24 01:04:45 -0400

    Attempt to make setup.py work with pkgconfig

--- setup.py
+++ setup.py
@@ -3,17 +3,64 @@ from setuptools import setup, Extension
 with open("README.md", "r") as f:
     long_description = f.read()
 
-def get_extensions():
-    import pkgconfig
+class PkgconfigExtensions:
+    def __init__(self, exts):
+        """Create PkgconfigExtensions from the given list of extension info.
+        Each extension must be a dict where keys are the arguments for
+        Extension() with one additional key, 'libs', a string to be passed to
+        pkgconfig.parse()."""
+        self.pkgconfig_ready = False
+        self.extensions = exts
 
-    return [
-        Extension('md4c',
-            sources=[
-                'src/pymd4c.c',
-            ],
-            **pkgconfig.parse('md4c')
-        ),
-    ]
+    def _fetch_pkgconfig(self):
+        # If pkgconfig already fetched, return
+        if self.pkgconfig_ready:
+            return
+
+        import pkgconfig
+
+        # Use 'libs' key to add pkgconfig info to all extensions
+        new_exts = []
+        for extension in self.extensions:
+            # If no 'libs' key, add as-is
+            try:
+                libs = extension['libs']
+            except KeyError:
+                new_exts.append(Extension(**extension))
+
+            # Add pkgconfig info
+            del extension['libs']
+            for k, v in pkgconfig.parse(libs).items():
+                try:
+                    extension[k].extend(v)
+                except KeyError:
+                    extension[k] = v
+            new_exts.append(Extension(**extension))
+
+        # Store transformed extension list
+        self.extensions = new_exts
+        self.pkgconfig_ready = True
+
+    def __iter__(self):
+        self._fetch_pkgconfig()
+        return iter(self.extensions)
+
+    def __len__(self):
+        return len(self.extensions)
+
+    def __getitem__(self, key):
+        self._fetch_pkgconfig()
+        return self.extensions[key]
+
+extensions = PkgconfigExtensions([
+    {
+        'name': 'md4c',
+        'sources': [
+            'src/pymd4c.c',
+        ],
+        'libs': 'md4c',
+    },
+])
 
 setup(
     name="PyMD4C",
@@ -26,8 +73,8 @@ setup(
     url="https://github.com/dominickpastore/pymd4c",
     setup_requires=[
         'pkgconfig',
-    ]
-    ext_modules=get_extensions(),
+    ],
+    ext_modules=extensions,
     classifiers=[
         "Programming Language :: C",
         "Programming Language :: Python :: 3",
