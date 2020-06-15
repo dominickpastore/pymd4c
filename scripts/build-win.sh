9dd9a9011f04ce6991d7d2257b47a69c1b1a6571
Dominick C. Pastore <dominickpastore@dcpx.org>
2020-06-15 13:57:17 -0400

    Try to add Windows to CI

---
+++ scripts/build-win.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e -u -x
+
+# Install MD4C
+mkdir md4c-lib/build
+(
+    cd md4c-lib/build
+    cmake3 ..
+    make
+    make install
+)
+
+python3 -m pip wheel . --no-deps -w wheelhouse/
