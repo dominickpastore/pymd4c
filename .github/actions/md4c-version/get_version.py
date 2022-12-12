d12955309025f44da228fb57cc915d174eab2a22
Dominick C. Pastore <dominickpastore@dcpx.org>
2022-12-11 22:52:11 -0500

    Attempt 3 at GitHub Actions fix

--- .github/actions/md4c-version/get_version.py
+++ .github/actions/md4c-version/get_version.py
@@ -1,9 +1,13 @@
+from __future__ import print_function
 import json
 import os
+
 with open('about.json') as f:
     about = json.load(f)
+
 min_version = about['md4c-version']['min']
 max_version = about['md4c-version']['max']
+
 with open(os.environ['GITHUB_OUTPUT'], 'w') as f:
     print('md4c=' + max_version, file=f)
     print('md4c-min=' + min_version, file=f)
